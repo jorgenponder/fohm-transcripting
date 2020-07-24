819cb76a238b3439af1c49cc526c8c78f1a70170
Jorgen Ponder <jorgenponder@example.com>
2020-07-24 14:36:20 +0200

    Each screenshot in its own array element

--- code/bin/collate-as-json.sh
+++ code/bin/collate-as-json.sh
@@ -2,9 +2,10 @@
 for d in */ ; do
     cd "$d"
     echo '[' > "../${d%/}tesseract-utskrift.json"
-    ls|grep txt.txt|sort|xargs cat |perl -n -e 's/[^a-zA-ZåäöÅÄÖ0-9.;, \-\n]//g;s/\n/\\n/g;print "\"$_\",\n"' >> "../${d%/}tesseract-utskrift.json"
+    for f in *.txt.txt; do
+        cat $f |perl -n -e 's/[^a-zA-ZåäöÅÄÖ0-9.;, \-\n]//g;s/\n/\\n/g;print "[\"$_\"],\n"' >> "../${d%/}tesseract-utskrift.json"
+    done
     echo '""]' >> "../${d%/}tesseract-utskrift.json"
-
     cd ..
     echo "$d utskrift done"
 done
